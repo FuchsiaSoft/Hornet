5bb5c553a31f35c81849af69c9236c84e5376cf4
Chris Wilson <FuchsiaSoft@users.noreply.github.com>
2017-07-29 18:32:13 +0100

    re-worked UI to make it all fit

--- Hornet/Properties/Resources.Designer.cs
+++ Hornet/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace Hornet.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo {
+            get {
+                object obj = ResourceManager.GetObject("Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon logo_UNf_icon {
+            get {
+                object obj = ResourceManager.GetObject("logo_UNf_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
